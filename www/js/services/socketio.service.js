1ce48f4cb454e43b9a81e26086a11c974f747d93
Breno Caldeira <breno.caldeira@gmail.com>
2016-03-12 14:40:41 -0300

    config services

---
+++ www/js/services/socketio.service.js
@@ -0,0 +1,11 @@
+(fucntion(){
+	angular.module('starter')
+	.service('SocketIOService', ['socketFactory', SocketIOService]);
+
+	function SocketIOService(socketFactory){
+
+		return socketFactory({
+			ioSocket: io.connect('https://nameless-cove-97938.herokuapp.com')
+		});
+	}
+})();
