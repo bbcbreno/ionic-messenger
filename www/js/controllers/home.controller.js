cf7d4e269120c7716ce05d839e17d53b819d7590
Breno Caldeira <breno.caldeira@gmail.com>
2016-03-12 14:50:16 -0300

    controller for home

---
+++ www/js/controllers/home.controller.js
@@ -0,0 +1,13 @@
+(function(){
+	angular.module('starter')
+	.controller('HomeController', ['$scope', '$state', 'localStorageService', 'SocketIOService', HomeController]);
+
+	function HomeController($scope, $state, localStorageService, SocketIOService){
+
+		$scope.login = function(username){
+			localStorageService.set('user', username);
+			SocketIOService.emit('chat:join', { user: username });
+			$state.go('chat')
+		};
+	}
+})();
